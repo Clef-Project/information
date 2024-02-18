d2e0e283307956b3587d45e2a864e321bb853173
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-02-18 13:50:52 +0900

    update 5 files and create 6 files

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+import Icon from "/public/klef_logo_b.svg"
+
+const Header_a = () => {
+  return(
+    <header className="p-0 m-0 bg-violet-50">
+      <Icon height={80} width={220} className="p-0 m-0"/>
+    </header>
+  )
+}
+
+export default Header_a;
